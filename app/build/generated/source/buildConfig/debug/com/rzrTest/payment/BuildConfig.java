6f383cbdb06772c4b2877979ba207b33aa3d30a7
Burra Kranthi Kiran <developer.startoonlabs@gmail.com>
2023-11-09 16:26:12 +0530

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/rzrTest/payment/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rzrTest.payment;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rzrTest.payment";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 3050004;
+  public static final String VERSION_NAME = "3.5.0.4";
+  // Field from default config.
+  public static final String ACCESS_KEY = "c6U5w2YQEP2DoazqatbyJU3nvIk5A47lX2/Am89SLXofAg==";
+  // Field from default config.
+  public static final String ATTESTATION_CERT_PINNING = "sha256/BJlJjxY7OHxhAz6yqy2gm58+qlP0AGwnBHDIG6zkhfU=";
+  // Field from default config.
+  public static final String ATTESTATION_HOST = "https://mpos-uat.fasspay.com:9001";
+  // Field from default config.
+  public static final String FLAVOR_environment = "uat";
+  // Field from default config.
+  public static final String GOOGLE_API_KEY = "jAUZd+2ryJoJrJivYgy81EbQWOhGQZEpnwnumWwpAig=";
+  // Field from default config.
+  public static final String SECRET_KEY = "v0aQsKfVVucQnxKE5AsMtHp/EYRpsuLIAp504MNGJY0=";
+}
